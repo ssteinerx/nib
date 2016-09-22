322a7356b2a77d0a3b0080290e34ec11a9baf1df
Steve Steiner <ssteinerX@gmail.com>
2016-09-22 09:11:44 -0400

    Update .gitignore from ssteinerX-updates branch

---
+++ nib/plugins/markdown-pp.py
@@ -0,0 +1,26 @@
+# coding=utf-8
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import markdown
+from nib import Processor, markup
+
+
+@markup(['.md', '.mkdn'])
+class MarkdownPP_Processor(Processor):
+    def __init__(self, options):
+        self.options = options
+        config = {
+            'markdown.extensions.footnotes': {
+                'UNIQUE_IDS': True,
+            },
+        }
+        self.markdown = markdown.Markdown(output_format='html5',
+                                          extensions=config.keys(),
+                                          extension_configs=config,
+                                          )
+
+    def document(self, document):
+        document.short = self.markdown.reset().convert(document.short)
+        document.content = self.markdown.reset().convert(document.content)
+        document.extension = '.html'
+        return document
