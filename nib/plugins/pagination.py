728225d31a21846bae47b547aee2dd70761239f9
Steve Steiner <ssteinerX@gmail.com>
2016-09-22 21:21:24 -0400

    Put away before pull from upstream.

--- nib/plugins/pagination.py
+++ nib/plugins/pagination.py
@@ -1,6 +1,5 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-from os import path
 try:
     from urllib.parse import urlparse, urlunparse, ParseResult
 except ImportError:
